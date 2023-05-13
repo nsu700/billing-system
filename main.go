77201a84e64c5dc43afa98bdc90c61c5434c0689
Nick Su <nicksu383@gmail.com>
2023-05-13 15:47:45 +0800

    Add a main page
    1. Main page display all spending
    2. After a success submission, automatically redirect to main page
    3. Reformat the submit page

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 
 	_ "modernc.org/sqlite"
 )
 
 var db *sql.DB
+
+type Spending struct {
+	Date        string  `json:"date"`
+	Amount      float64 `json:"amount"`
+	Description string  `json:"description"`
+	Type        string  `json:"type"`
+}
 
 func main() {
 	var err error
@@ -18,7 +25,7 @@ func main() {
 	// Get the path of the SQLite database file from the environment variable
 	dbPath := os.Getenv("DB_PATH")
 	if dbPath == "" {
-		dbPath = "./mydb.db"
+		dbPath = "./billing-system.db"
 	}
 
 	db, err = sql.Open("sqlite", dbPath)
@@ -28,12 +35,13 @@ func main() {
 	defer db.Close()
 
 	// Create the table if it doesn't exist
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS mytable (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, amount INTEGER, type TEXT, purpose TEXT)")
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS mytable (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, amount INTEGER, type TEXT, description TEXT)")
 	if err != nil {
 		log.Fatalf("Failed to create table: %v", err)
 	}
 
 	http.HandleFunc("/submit", submitHandler)
+	http.HandleFunc("/add", addDataHandler)
 	http.HandleFunc("/", indexHandler)
 
 	log.Println("Server listening on port 8080...")
@@ -56,19 +64,19 @@ func submitHandler(w http.ResponseWriter, r *http.Request) {
 	dates := r.Form["date[]"]
 	amounts := r.Form["amount[]"]
 	types := r.Form["type[]"]
-	purposes := r.Form["purpose[]"]
+	descriptions := r.Form["description[]"]
 
 	// Loop through the arrays of values for each input field
 	for i := 0; i < len(dates); i++ {
 		// Insert the values into the database
-		stmt, err := db.Prepare("INSERT INTO mytable (date, amount, type, purpose) VALUES (?, ?, ?, ?)")
+		stmt, err := db.Prepare("INSERT INTO mytable (date, amount, type, description) VALUES (?, ?, ?, ?)")
 		if err != nil {
 			log.Printf("Failed to prepare statement: %v", err)
 			return
 		}
 		defer stmt.Close()
 
-		_, err = stmt.Exec(dates[i], amounts[i], types[i], purposes[i])
+		_, err = stmt.Exec(dates[i], amounts[i], types[i], descriptions[i])
 		if err != nil {
 			log.Panicf("Failed to execute statement: %v", err)
 			return
@@ -76,10 +84,69 @@ func submitHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Respond with a success message
-	fmt.Fprintln(w, "Form submission received and written to database!")
+	http.Redirect(w, r, "/", http.StatusFound)
 	log.Println("Form submission received and written to database.")
 }
 
+func addDataHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "submit-form.html")
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "index.html")
+	// Check if there is any spending data in the database
+	var count int
+	err := db.QueryRow("SELECT COUNT(*) FROM mytable").Scan(&count)
+	if err != nil {
+		log.Printf("Failed to execute query: %v", err)
+		http.Error(w, "Failed to fetch spending data", http.StatusInternalServerError)
+		return
+	}
+
+	if count == 0 {
+		// If there is no spending data, display the submit page
+		http.ServeFile(w, r, "submit-form.html")
+		return
+	}
+
+	// Fetch the spending data from the database
+	rows, err := db.Query("SELECT date, amount, type, description FROM mytable")
+	if err != nil {
+		log.Printf("Failed to execute query: %v", err)
+		http.Error(w, "Failed to fetch spending data", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var spendings []Spending
+	for rows.Next() {
+		var spending Spending
+		err := rows.Scan(&spending.Date, &spending.Amount, &spending.Type, &spending.Description)
+		if err != nil {
+			log.Printf("Failed to scan row: %v", err)
+			continue
+		}
+		spendings = append(spendings, spending)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Printf("Error while iterating rows: %v", err)
+		http.Error(w, "Failed to fetch spending data", http.StatusInternalServerError)
+		return
+	}
+
+	// Parse the HTML template
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Printf("Failed to parse HTML template: %v", err)
+		http.Error(w, "Failed to parse HTML template", http.StatusInternalServerError)
+		return
+	}
+
+	// Execute the HTML template with the spending data
+	err = tmpl.Execute(w, spendings)
+	if err != nil {
+		log.Printf("Failed to execute HTML template: %v", err)
+		http.Error(w, "Failed to execute HTML template", http.StatusInternalServerError)
+		return
+	}
 }
